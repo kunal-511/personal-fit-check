9199f711292fa89285c6f12a2bd337cf2a176ec2
Kunal Dugar <yoyokvunal@gmail.com>
2026-01-28 15:43:31 +0530

    v1.1.1

---
+++ scripts/postgress-up.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+docker run --name local-postgres \
+  -e POSTGRES_USER=postgres \
+  -e POSTGRES_PASSWORD=password \
+  -e POSTGRES_DB=fitness_tracker \
+  -p 5432:5432 \
+  -d postgres:16
+
+sleep 15
+
+cd ../db
+
+psql \
+  -h localhost \
+  -p 5432 \
+  -U postgres \
+  -d fitness_tracker \
+  -f schema.sql
+
+echo "PostgreSQL is running on port 5432"
